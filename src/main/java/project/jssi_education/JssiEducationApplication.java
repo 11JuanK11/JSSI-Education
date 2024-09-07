ffb98ff64cc3ee3af50e3ce2c0927c30163b9bea
JuanK <camiloalzatebedoya15@gmail.com>
2024-09-07 18:26:45 -0500

    Create new project
    Initialized project with basic configs

---
+++ src/main/java/project/jssi_education/JssiEducationApplication.java
@@ -0,0 +1,13 @@
+package project.jssi_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JssiEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JssiEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/jssi_education/JssiEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package project.jssi_education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JssiEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
