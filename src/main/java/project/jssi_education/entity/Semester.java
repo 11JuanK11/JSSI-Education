e260d9fa5ea226b00f930f47f1accb09e6c7e402
SaraSanchez <sara_sanchez82221@elpoli.edu.co>
2024-09-17 14:06:39 -0500

    add new entity
    add new entity semester with their relationships

---
+++ src/main/java/project/jssi_education/entity/Semester.java
@@ -0,0 +1,17 @@
+package project.jssi_education.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+public class Semester {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int Semester;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private Set<Course> courses;
+}
