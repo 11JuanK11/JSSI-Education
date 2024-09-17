ac1c99af687699d0619aca9f1f0f09760353476e
IsaMontoya17 <isabelamontoya2021@gmail.com>
2024-09-17 12:59:08 -0500

    Add relationships between entities
    Implement relationships in some entities

---
+++ src/main/java/project/jssi_education/entity/EmploymentContract.java
@@ -0,0 +1,23 @@
+package project.jssi_education.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class EmploymentContract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String contractType;
+    private String status;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id", nullable = false)
+    private Manager manager;
+}
--- src/main/java/project/jssi_education/entity/Teacher.java
+++ src/main/java/project/jssi_education/entity/Teacher.java
@@ -1,9 +1,6 @@
 package project.jssi_education.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -15,4 +12,8 @@ public class Teacher{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String specialization;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "employment_contract_id")
+    private EmploymentContract employmentContract;
 }
