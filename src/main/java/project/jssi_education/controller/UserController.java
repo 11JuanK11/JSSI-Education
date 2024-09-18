04c7678adb22b4aca6d464c81e6d7131366c7f0f
JuanK <camiloalzatebedoya15@gmail.com>
2024-09-18 16:08:16 -0500

    Add methods to repository classes
    Add new methods in repository classes.

--- src/main/java/project/jssi_education/controller/UserController.java
+++ src/main/java/project/jssi_education/controller/UserController.java
@@ -1,13 +1,38 @@
 package project.jssi_education.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import project.jssi_education.entity.User;
 import project.jssi_education.service.IUserService;
+import project.jssi_education.service.impl.UserService;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     @Autowired
-    private IUserService userService;
+    private UserService userService;
+
+    @GetMapping("/")
+    public List<User> findAll(){
+        return userService.FindAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findOne(@PathVariable Long id){
+        return userService.FindbyId(id);
+    }
+
+    @PostMapping("/")
+    public void insert(@RequestBody User user){
+        userService.Insert(user);
+    }
+
+
+
+
+
 }
