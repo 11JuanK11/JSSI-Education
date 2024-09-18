ef83aadb79877a04d1ed18ecf995d42804a32703
JuanK <camiloalzatebedoya15@gmail.com>
2024-09-18 16:00:18 -0500

    Add methods to service classes
    Included new methods in service classes.

--- src/main/java/project/jssi_education/service/impl/StudentService.java
+++ src/main/java/project/jssi_education/service/impl/StudentService.java
@@ -1,8 +1,34 @@
 package project.jssi_education.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import project.jssi_education.entity.Student;
+import project.jssi_education.entity.User;
+import project.jssi_education.repository.IStudentRepository;
+import project.jssi_education.repository.IUserRepository;
 import project.jssi_education.service.IStudentService;
+
+import java.util.List;
 
 @Service
 public class StudentService implements IStudentService {
+    @Autowired
+    private IStudentRepository repo;
+
+    public Student FindbyId(Long id){
+        for (Student student: repo.findAll()){
+            if (student.getId().equals(id)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> FindAll(){
+        return repo.findAll();
+    }
+
+    public void Insert(Student student){
+        repo.save(student);
+    }
 }
