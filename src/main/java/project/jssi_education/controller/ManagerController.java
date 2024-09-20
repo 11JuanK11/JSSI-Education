f7f863ba7c2325d3ab205d5728605143b630eec9
SaraSanchez <sara_sanchez82221@elpoli.edu.co>
2024-09-19 20:43:24 -0500

    Implement CRUD functionality
    Add CRUD functionality for Manager

--- src/main/java/project/jssi_education/controller/ManagerController.java
+++ src/main/java/project/jssi_education/controller/ManagerController.java
@@ -1,13 +1,82 @@
 package project.jssi_education.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.jssi_education.entity.Manager;
+import project.jssi_education.entity.User;
+import project.jssi_education.exception.ResourceNotFoundException;
 import project.jssi_education.service.IManagerService;
+import project.jssi_education.service.impl.UserService;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/managers")
 public class ManagerController {
     @Autowired
     private IManagerService managerService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public List<Manager> findAll(){
+        return managerService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findOne(@PathVariable Long id) {
+        try {
+            Manager manager = managerService.findById(id);
+            return new ResponseEntity<>(manager, HttpStatus.OK);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(Map.of("message", ex.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(Map.of("message", "Internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> insert(@RequestBody Manager manager) {
+        try {
+            if (manager.getUser() == null || manager.getUser().getId() == null) {
+                return new ResponseEntity<>("User information is missing.", HttpStatus.BAD_REQUEST);
+            }
+            User userAux = userService.FindbyId(manager.getUser().getId());
+            manager.setUser(userAux);
+            managerService.insert(manager);
+            return new ResponseEntity<>("Manager successfully created.", HttpStatus.CREATED);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>("An error occurred while creating the manager.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            managerService.deleteById(id);
+            return new ResponseEntity<>(Map.of("message", "Manager successfully deleted."), HttpStatus.NO_CONTENT);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(Map.of("message", ex.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(Map.of("message", "Internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@PathVariable Long id, @RequestBody Manager manager) {
+        try {
+            managerService.update(id, manager);
+            return new ResponseEntity<>("Manager successfully updated.", HttpStatus.OK);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>("An error occurred while updating the student.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/project/jssi_education/service/IManagerService.java
+++ src/main/java/project/jssi_education/service/IManagerService.java
@@ -1,4 +1,14 @@
 package project.jssi_education.service;
 
+import project.jssi_education.entity.Manager;
+import project.jssi_education.exception.ResourceNotFoundException;
+
+import java.util.List;
+
 public interface IManagerService {
+    public Manager findById(Long id) throws ResourceNotFoundException;
+    public List<Manager> findAll();
+    public void insert(Manager manager) throws ResourceNotFoundException;
+    public void deleteById(Long id) throws ResourceNotFoundException;
+    public void update(Long id, Manager manager) throws ResourceNotFoundException;
 }
