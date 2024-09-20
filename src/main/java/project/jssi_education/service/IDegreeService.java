02a3f08ed768842c7a90d4533b76e56679a6e2bd
Sara-Castaneda25 <saracastanedaecheverri@gmail.com>
2024-09-20 09:18:18 -0500

    implement methods
    implement methods in IDegreeService

--- src/main/java/project/jssi_education/service/IDegreeService.java
+++ src/main/java/project/jssi_education/service/IDegreeService.java
@@ -1,4 +1,11 @@
 package project.jssi_education.service;
 
+import project.jssi_education.entity.Degree;
+
+import java.util.List;
+
 public interface IDegreeService {
+    public Degree FindbyId(Long id);
+    public List<Degree> FindAll();
+    public void Insert(Degree degree);
 }
