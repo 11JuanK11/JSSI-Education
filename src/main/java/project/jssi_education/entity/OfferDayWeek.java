adbb774045ba96df9d2eddccd20d7ba42b82af40
JuanK <camiloalzatebedoya15@gmail.com>
2024-09-21 19:23:15 -0500

    Create OfferDayWeek class
    Add new intermediate class OfferDayWeek to manage relationships.

---
+++ src/main/java/project/jssi_education/entity/OfferDayWeek.java
@@ -0,0 +1,22 @@
+package project.jssi_education.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.generator.values.GeneratedValues;
+
+@Data
+@Entity
+@Table(name = "offer_day_week")
+public class OfferDayWeek {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id", nullable = false)
+    private Offer offer;
+
+    @ManyToOne
+    @JoinColumn(name = "day_week_id", nullable = false)
+    private DayWeek dayWeek;
+}
