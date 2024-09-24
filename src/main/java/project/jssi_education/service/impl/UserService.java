e4199061df99547a5b957aa207d03da4c6eca1db
JuanK <camiloalzatebedoya15@gmail.com>
2024-09-24 11:42:20 -0500

    Refactor User classes (backend)
    Rename methods in User repository, service and controller

--- src/main/java/project/jssi_education/service/impl/UserService.java
+++ src/main/java/project/jssi_education/service/impl/UserService.java
@@ -1,6 +1,7 @@
 package project.jssi_education.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import project.jssi_education.entity.User;
 import project.jssi_education.repository.IUserRepository;
@@ -12,8 +13,11 @@ import java.util.List;
 public class UserService implements IUserService{
     @Autowired
     private IUserRepository repo;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @Override
-    public User FindbyId(Long id){
+    public User findbyId(Long id){
         for (User user: repo.findAll()){
             if (user.getId().equals(id)){
                 return user;
@@ -22,11 +26,11 @@ public class UserService implements IUserService{
         return null;
     }
     @Override
-    public List<User> FindAll(){
+    public List<User> findAll(){
         return repo.findAll();
     }
     @Override
-    public void Insert(User user){
+    public void insert(User user){
         repo.save(user);
     }
 
