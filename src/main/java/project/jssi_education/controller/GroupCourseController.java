0f2a4c06a69201ca8b07c5ef498e6d61130a866d
JuanK <camiloalzatebedoya15@gmail.com>
2024-09-24 22:10:50 -0500

    Add CRUD to GroupCourse(Backend)
    Implement CRUD operations for GroupCourse entity.

--- src/main/java/project/jssi_education/controller/GroupCourseController.java
+++ src/main/java/project/jssi_education/controller/GroupCourseController.java
@@ -1,4 +1,72 @@
 package project.jssi_education.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.jssi_education.entity.GroupCourse;
+import project.jssi_education.exception.ResourceNotFoundException;
+import project.jssi_education.service.IGroupCourseService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/group-course/")
 public class GroupCourseController {
+    @Autowired
+    private IGroupCourseService groupCourseService;
+
+    @GetMapping("/")
+    public List<GroupCourse> findAll(){
+        return groupCourseService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findOne(@PathVariable Long id) {
+        try {
+            GroupCourse groupCourse = groupCourseService.findById(id);
+            return new ResponseEntity<>(groupCourse, HttpStatus.OK);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(Map.of("message", ex.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(Map.of("message", "Internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> insert(@RequestBody GroupCourse groupCourse) {
+        try {
+            groupCourseService.insert(groupCourse);
+            return new ResponseEntity<>("Group Course successfully created.", HttpStatus.CREATED);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>("An error occurred while creating the Group Course.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        try {
+            groupCourseService.deleteById(id);
+            return new ResponseEntity<>(Map.of("message", "Group Course successfully deleted."), HttpStatus.NO_CONTENT);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(Map.of("message", ex.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(Map.of("message", "Internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> Update(@PathVariable Long id, @RequestBody GroupCourse groupCourse) {
+        try {
+            groupCourseService.update(id, groupCourse);
+            return new ResponseEntity<>("Group Course successfully updated.", HttpStatus.OK);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>("An error occurred while updating the Group Course.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/project/jssi_education/service/impl/GroupCourseService.java
+++ src/main/java/project/jssi_education/service/impl/GroupCourseService.java
@@ -1,6 +1,76 @@
 package project.jssi_education.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+import project.jssi_education.entity.GroupCourse;
+import project.jssi_education.exception.ResourceNotFoundException;
+import project.jssi_education.repository.ICourseRepository;
+
+import project.jssi_education.repository.IGroupCourseRepository;
+import project.jssi_education.repository.IGroupRepository;
 import project.jssi_education.service.IGroupCourseService;
 
+import java.util.List;
+@Service
 public class GroupCourseService implements IGroupCourseService {
+    @Autowired
+    private IGroupCourseRepository groupCourseRepository;
+    @Autowired
+    private ICourseRepository courseRepository;
+    @Autowired
+    private IGroupRepository groupRepository;
+
+    @Override
+    public GroupCourse findById(Long id) throws ResourceNotFoundException {
+        return groupCourseRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Group Course not found with id: " + id));
+    }
+
+    @Override
+    public List<GroupCourse> findAll() {
+        return groupCourseRepository.findAll();
+    }
+
+    @Override
+    public void insert(GroupCourse groupCourse) throws ResourceNotFoundException {
+        if(groupCourse.getCourse() == null || groupCourse.getGroup() == null){
+            throw new ResourceNotFoundException("Group Course information is missing.");
+        }
+        groupCourseRepository.save(groupCourse);
+    }
+
+    @Override
+    public void deleteById(Long id) throws ResourceNotFoundException {
+        if (!groupCourseRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Group Course not found with id: " + id);
+        }
+        groupCourseRepository.deleteById(id);
+
+    }
+
+    @Override
+    public void update(Long id, GroupCourse groupCourse) throws ResourceNotFoundException {
+        GroupCourse existingGroupCourse = findById(id);
+        if (groupCourse.getCourse() != null || groupCourse.getGroup() != null || groupCourse.getAttendances() != null || groupCourse.getGrades() != null || groupCourse.getTeacher_evaluations() != null){
+            if(groupCourse.getCourse() != null){
+                existingGroupCourse.setCourse(courseRepository.getReferenceById(groupCourse.getCourse().getCourseId()));
+            }
+            if (groupCourse.getGroup() != null){
+                existingGroupCourse.setGroup(groupRepository.getReferenceById(groupCourse.getGroup().getGroupId()));
+            }
+            if(groupCourse.getAttendances() != null){
+                existingGroupCourse.setAttendances(groupCourse.getAttendances());
+            }
+            if(groupCourse.getGrades() != null){
+                existingGroupCourse.setGrades(groupCourse.getGrades());
+            }
+            if(groupCourse.getTeacher_evaluations()!= null){
+                existingGroupCourse.setTeacher_evaluations(groupCourse.getTeacher_evaluations());
+            }
+        }else{
+            throw new ResourceNotFoundException("Group Course information is missing");
+        }
+        groupCourseRepository.save(groupCourse);
+    }
 }
