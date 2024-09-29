9393c0476c751f0ab9d010ae57e36a7d662c229b
JuanK <camiloalzatebedoya15@gmail.com>
2024-09-29 16:24:00 -0500

    Update login with teacher dashboard
    Link login functionality to redirect teachers to their dashboard after successful authentication.

--- src/main/java/project/jssi_education/service/impl/UserService.java
+++ src/main/java/project/jssi_education/service/impl/UserService.java
@@ -3,6 +3,7 @@ package project.jssi_education.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.jssi_education.entity.User;
+import project.jssi_education.exception.ResourceNotFoundException;
 import project.jssi_education.repository.IUserRepository;
 import project.jssi_education.service.IUserService;
 import project.jssi_education.util.PasswordUtil;
@@ -27,10 +28,18 @@ public class UserService implements IUserService {
     public List<User> findAll(){
         return userRepository.findAll();
     }
+
     @Override
-    public User insert(User user) {
+    public User insert(User user) throws ResourceNotFoundException {
         user.setPassword(PasswordUtil.hashPassword(user.getPassword()));
+        List<User> users = findAll();
+        for (User existingUser: users){
+            if(existingUser.getUserName().equalsIgnoreCase(user.getUserName()) || existingUser.getEmail().equalsIgnoreCase(user.getEmail()) || existingUser.getIdNumber() == user.getIdNumber()){
+                throw new ResourceNotFoundException("User information is existing.");
+            }
+        }
         return userRepository.save(user);
+
     }
 
     @Override
